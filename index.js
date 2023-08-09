5c0bcd874471c9070161c16494acf7190a541bc1
AjBarton7 <ajenebarton1@gmail.com>
2023-08-09 06:19:19 +0000

    moving

--- index.js
+++ index.js
@@ -1,31 +1,168 @@
 kaboom({
-    width:1280,
-    height:720
+    width: 1280,
+    height: 720
 })
 
-loadSprite('background-1','./images/background1.jpg')
+loadSprite('background-1', './images/background1.jpg')
+
+
 loadSprite('lamp', './images/Character 03/Png/Lamp_post.png')
+
+loadSprite('idle-sprite', './images/mark.png', {
+    sliceX: 8,
+    sliceY: 1,
+    anims: { 'idle-anim': { from: 0, to: 7, loop: true }}
+})
+
+loadSprite('run-sprite', './images/mark.png', {
+    sliceX: 8,
+    sliceY: 1,
+    anims: { 'run-anim': { from: 0, to: 7, loop: true }}
+})
+
+
+loadSpriteAtlas('./images/Tiles.png', {
+    'platform-left': {
+        x: 440,
+        y: 380,
+        width: 32,
+        height: 32
+    },
+    'platform-middle': {
+        x: 520,
+        y: 240,
+        width: 32,
+        height: 32
+    },
+})
+
+
+setGravity(1000)
 
 add([
     sprite('background-1'),
     fixed(),
     scale(4)
-    ])
-    
+])
+
 add([
     sprite('lamp'),
     fixed(),
     scale(4),
     pos(50, 210)
-    ])
-    
-    add([
+])
+
+add([
     sprite('lamp'),
     fixed(),
     scale(4),
     pos(1000, 210)
-    ]).flipX = true
-    
-    // loadSpriteAtlas()
-    
-    
+]).flipX = true
+
+
+// console.log(atlas)
+
+const map = addLevel([
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                 111                  5',
+        '5          111                         5',
+        '5                                      5',
+        '5   111           1            1       5',
+        '5                                      5',
+        '1111111111111111111111111111111111111111',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+        '5                                      5',
+
+
+    ],
+
+    {
+        tileWidth: 32,
+        tileHeight: 32,
+        tiles: {
+            5: () => [
+                 rect(16, 16),
+            opacity(0),
+            area(),
+            body({isStatic: true})
+            ],
+
+            1: () => [
+                sprite('platform-middle'),
+                area(),
+                body({ isStatic: true })
+            ],
+
+
+        }
+
+    })
+
+const player = add([
+    sprite('idle-sprite'),
+    scale(1),
+    area({shape: new Rect(vec2(0), 16, 16), offset: vec2(0,16)}),
+    anchor('center'),
+    body(),
+    pos(80,210),
+    {
+        speed: 500,
+        previousHeight: null,
+        heightDelta: 0,
+        direction: 'right'
+    }
+])
+
+player.play('idle-anim')
+
+onKeyDown('right', () => {
+    if (player.curAnim() !== 'run-anim' && player.isGrounded()) {
+        player.use(sprite('run-sprite'))
+        player.play('run-anim')
+    }
+
+    if (player.direction !== 'right') player.direction = 'right'
+
+    player.move(player.speed, 0)
+})
+
+onKeyRelease('right', () => {
+    player.use(sprite('idle-sprite'))
+    player.play('idle-anim')
+})
+
+onKeyDown('left', () => {
+    if (player.curAnim() !== 'run-anim' && player.isGrounded()) {
+        player.use(sprite('run-sprite'))
+        player.play('run-anim')
+    }
+
+    if (player.direction !== 'left') player.direction = 'left'
+
+    player.move(-player.speed, 0)
+})
+
+onKeyRelease('left', () => {
+    player.use(sprite('idle-sprite'))
+    player.play('idle-anim')
+})
+
+onKeyPress('up', () => {
+    if (player.isGrounded()) {
+        player.jump()
+    }
+})
